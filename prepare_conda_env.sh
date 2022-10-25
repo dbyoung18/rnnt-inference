a61f99bec5ae24c66f153cda3f7c991a3e10c548
Lai, Yejing <yejing.lai@intel.com>
2022-10-24 21:55:59 -0700

    fix end2end process

--- prepare_conda_env.sh
+++ prepare_conda_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create new env and activate it
-conda install -y python==3.8
 conda install -y ninja cmake jemalloc inflect libffi pandas requests toml tqdm unidecode
 conda install -c intel -y mkl mkl-include intel-openmp
 conda install -c conda-forge -y llvm-openmp librosa
+pip install -y sox
